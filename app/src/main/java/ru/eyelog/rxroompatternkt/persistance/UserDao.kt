b02ad026dc8bfd0196455719e3ce92d7cf6f2c4b
lisivenkoma <lisivenkoma@sibur.ru>
2019-05-14 00:38:37 +0300

    Coding is going

--- app/src/main/java/ru/eyelog/rxroompatternkt/persistance/UserDao.kt
+++ app/src/main/java/ru/eyelog/rxroompatternkt/persistance/UserDao.kt
@@ -10,12 +10,23 @@ import io.reactivex.Flowable
 @Dao
 interface UserDao{
 
-    @Query("SELECT * FROM users WHERE userId = :id")
-    fun getUserById(id:String):Flowable<User>
-
+    // Create and also update user
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertUser(user: User):Completable
 
+    // Get all users
+    @Query("SELECT * FROM users")
+    fun getAllUsers():Flowable<List<User>>
+
+    // Get one user by id
+    @Query("SELECT * FROM users WHERE userId = :id")
+    fun getUserById(id:String):Flowable<User>
+
+    // Delete one user by id
+    @Query("DELETE FROM users WHERE userId = :id")
+    fun deleteOneUserById(id: String)
+
+    // Delete all users
     @Query("DELETE FROM users")
     fun deleteAllUsers()
 }
